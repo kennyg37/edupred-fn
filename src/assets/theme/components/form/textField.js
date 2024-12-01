ab2356d00278b30b09dc5f6cac06ba49ae7e1232
kennyg37 <k.ganza@alustudent.com>
2024-12-01 23:35:21 +0200

    frontend

---
+++ src/assets/theme/components/form/textField.js
@@ -0,0 +1,12 @@
+
+import colors from "assets/theme/base/colors";
+
+const { transparent } = colors;
+
+export default {
+  styleOverrides: {
+    root: {
+      backgroundColor: transparent.main,
+    },
+  },
+};
